3cf9e47fd4d9f381530509ebbcbfd8600672fa39
akaplyar <bizizib60@gmail.com>
2017-03-27 02:46:30 +0300

    Initial commit

---
+++ ft_printf.h
@@ -0,0 +1,12 @@
+#ifndef FT_PRINTF_H
+# define FT_PRINTF_H
+
+# include "libft/libft.h"
+# include <unistd.h>
+# include <stdlib.h>
+# include <stdint.h>
+# include <stdarg.h>
+# include <limits.h>
+
+int         ft_printf(const char *format, ...);
+#endif
